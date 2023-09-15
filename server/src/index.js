8de962c375baaf8619a2d2e44429a8e0acde954d
penjorkinley <penjorkinley77@gmail.com>
2023-09-15 14:00:44 +0600

    set up backend and tailwindcss

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,12 @@
- 
+import express from "express";
+import cors from "cors";
+import moongoose from "mongoose";
+
+const app = express();
+const port = 3001;
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+  console.log("Server is running on port 3001");
+});
