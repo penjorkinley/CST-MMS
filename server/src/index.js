00044ab76ef18069e2823c1263065e3f510a1982
penjorkinley <penjorkinley77@gmail.com>
2023-09-15 15:44:21 +0600

    connected with mongoDB

--- server/src/index.js
+++ server/src/index.js
@@ -4,9 +4,23 @@ import moongoose from "mongoose";
 
 const app = express();
 const port = 3001;
+
 app.use(express.json());
 app.use(cors());
 
+moongoose.connect(
+  "mongodb+srv://CST-MMS:CST-MMS2023@cst-mss.wlq5y1g.mongodb.net/CST-MMS?retryWrites=true&w=majority"
+);
+
+const db = moongoose.connection;
+db.on("error", (error) => {
+  console.error("MongoDB connection error:", error);
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
 app.listen(port, () => {
-  console.log("Server is running on port 3001");
+  console.log("Server is running on port 3005");
 });
