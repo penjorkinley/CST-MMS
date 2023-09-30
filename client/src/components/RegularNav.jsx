aeb5e3759796b41e46f0f73e969b0b5d1a8631dd
karzeydema <110452208+karzeydema@users.noreply.github.com>
2023-09-30 15:48:52 +0600

    updated

--- client/src/components/RegularNav.jsx
+++ client/src/components/RegularNav.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-function RegularNav() {
+export default function RegularNav() {
   return (
-<<<<<<< HEAD
     <nav className="bg-transparent p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center"> {/* Container for logo and navigation */}
           <div>
-            <img src="client\src\assets\LogIn.png" alt="Logo" className="box-border h-32 w-32 p-4 border-4 ..." /> 
+            <img src="" alt="Logo" className="box-border h-32 w-32 p-4 border-4 ..." /> 
           </div>
           
         </div>
@@ -20,10 +19,4 @@ function RegularNav() {
       </div>
     </nav>
   );
-=======
-    <div>RegularNav</div>
-  )
->>>>>>> 81e737bd3cb3aca6d1b5b65ecf9eda370c980863
 }
-
-export default RegularNav
