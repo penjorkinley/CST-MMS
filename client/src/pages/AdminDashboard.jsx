3ab0d75994b2cf8e7ccfa847dedcb18f142d43a9
Kezang Loday <02200150.cst@rub.edu.bt>
2023-10-01 01:20:17 +0600

    nav updates

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,20 +1,38 @@
-import React from 'react'
-
+import React from "react";
+import AdminNavbar from "../components/AdminNavbar";
+import AdminTopNavbar from "../components/AdminTopNavbar";
 
 export default function AdminDashboard() {
   return (
-    <div className='flex justify-between items-center bg-cute p-4 '>
+    <div className="flex">
+      <div className="w-1/5">
+        {" "}
+        {/* Adjust the width as per your design */}
+        <AdminNavbar />
+      </div>
+      <div className="w-4/5">
+        {" "}
+        {/* Adjust the width as per your design */}
+        <AdminTopNavbar />
+      </div>
+      <div className="justify-between items-center bg-cute p-4 ">
+        <div className="card card-compact w-4/12 h-4/12 bg-base-100 shadow-2xl mr-24 mb-10">
+          {/* <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure> */}
+          <div className="card-body ">
+            <br></br>
+            <h2 className="card-title font-semibold text-black text-2xl italic">
+              Rate Your Experience!!!
+            </h2>
+            <br></br>
 
-    <div className="card card-compact w-4/12 h-4/12 bg-base-100 shadow-2xl mr-24 mb-10">
-  {/* <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure> */}
-  <div className="card-body "><br></br>
-    <h2 className="card-title font-semibold text-black text-2xl italic">Rate Your Experience!!!</h2><br></br>
-  
-    <div className="card-actions justify-end pb-10">
-      <button className="btn bg-black pl-9 w-full max-w-s font-semibold text-white text-xl">Submit</button>
+            <div className="card-actions justify-end pb-10">
+              <button className="btn bg-black pl-9 w-full max-w-s font-semibold text-white text-xl">
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-</div>
-  )
-};
+  );
+}
