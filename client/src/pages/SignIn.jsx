4c4655c67e473b53749dbee99df8fade7972998b
penjorkinley <penjorkinley77@gmail.com>
2023-09-30 15:53:44 +0600

    update signin page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -42,99 +42,103 @@ function SignIn({ changePage }) {
   };
 
   return (
-    <div className="flex h-screen bg-gray-100">
-      <div className="m-auto w-1/2 bg-ABB7BA p-5 rounded-lg shadow-md relative">
+    <div className="flex h-screen bg-F0F7F9">
+      <div className="w-1/2 flex justify-center items-center">
+        <div className="bg-ABB7BA p-5 rounded-lg shadow-md relative max-w-md w-full">
+          <h2 className="text-2xl font-bold mb-10 text-center">Sign In</h2>
+
+          {/* Input Fields */}
+          <div className="mb-4 relative">
+            <UserIcon className="absolute w-6 h-6 left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+            <input
+              className="pl-10 w-full px-3 py-2 placeholder-gray-300 border rounded-md bg-white"
+              placeholder="Name"
+              value={formData.name}
+              onChange={(e) =>
+                setFormData({ ...formData, name: e.target.value })
+              }
+            />
+            {errors.name && (
+              <div className="text-red-500 text-sm">{errors.name}</div>
+            )}
+          </div>
+
+          <div className="mb-4 relative">
+            <IdentificationIcon className="absolute w-6 h-6 left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+            <input
+              className="pl-10 w-full px-3 py-2 placeholder-gray-300 border rounded-md bg-white"
+              placeholder="Student ID"
+              value={formData.studentID}
+              onChange={(e) =>
+                setFormData({ ...formData, studentID: e.target.value })
+              }
+            />
+            {errors.studentID && (
+              <div className="text-red-500 text-sm">{errors.studentID}</div>
+            )}
+          </div>
+
+          <div className="mb-4 relative">
+            <PhoneIcon className="absolute w-6 h-6 left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+            <input
+              className="pl-10 w-full px-3 py-2 placeholder-gray-300 border rounded-md bg-white"
+              placeholder="Phone Number"
+              value={formData.phoneNumber}
+              onChange={(e) =>
+                setFormData({ ...formData, phoneNumber: e.target.value })
+              }
+            />
+            {errors.phoneNumber && (
+              <div className="text-red-500 text-sm">{errors.phoneNumber}</div>
+            )}
+          </div>
+
+          <div className="mb-4 relative">
+            <MailIcon className="absolute w-6 h-6 left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+            <input
+              className="pl-10 w-full px-3 py-2 placeholder-gray-300 border rounded-md bg-white"
+              placeholder="Email"
+              value={formData.email}
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
+            />
+            {errors.email && (
+              <div className="text-red-500 text-sm">{errors.email}</div>
+            )}
+          </div>
+
+          <div className="mb-4 relative">
+            <LockClosedIcon className="absolute w-6 h-6 left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+            <input
+              className="pl-10 w-full px-3 py-2 placeholder-gray-300 border rounded-md bg-white"
+              type="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={(e) =>
+                setFormData({ ...formData, password: e.target.value })
+              }
+            />
+            {errors.password && (
+              <div className="text-red-500 text-sm">{errors.password}</div>
+            )}
+          </div>
+
+          <button
+            onClick={handleSubmit}
+            className="w-full p-2 bg-61BDD5 text-black rounded-md"
+          >
+            Sign Up
+          </button>
+        </div>
+      </div>
+
+      <div className="w-1/2 relative">
         <img
           src={Logo}
           alt="Logo"
           className="absolute top-5 right-5 w-16 h-16"
         />
-        <h2 className="text-2xl font-bold mb-10 text-center">Sign In</h2>
-
-        <div className="mb-4 relative">
-          <UserIcon className="absolute w-6 h-6 left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
-          <input
-            className="pl-10 w-full px-3 py-2 placeholder-gray-300 border rounded-md bg-F0F7F9"
-            placeholder="Name"
-            value={formData.name}
-            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-          />
-          {errors.name && (
-            <div className="text-red-500 text-sm">{errors.name}</div>
-          )}
-        </div>
-
-        <div className="mb-4 relative">
-          <IdentificationIcon className="absolute w-6 h-6 left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
-          <input
-            className="pl-10 w-full px-3 py-2 placeholder-gray-300 border rounded-md bg-F0F7F9"
-            placeholder="Student ID"
-            value={formData.studentID}
-            onChange={(e) =>
-              setFormData({ ...formData, studentID: e.target.value })
-            }
-          />
-          {errors.studentID && (
-            <div className="text-red-500 text-sm">{errors.studentID}</div>
-          )}
-        </div>
-
-        <div className="mb-4 relative">
-          <PhoneIcon className="absolute w-6 h-6 left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
-          <input
-            className="pl-10 w-full px-3 py-2 placeholder-gray-300 border rounded-md bg-F0F7F9"
-            placeholder="Phone Number"
-            value={formData.phoneNumber}
-            onChange={(e) =>
-              setFormData({ ...formData, phoneNumber: e.target.value })
-            }
-          />
-          {errors.phoneNumber && (
-            <div className="text-red-500 text-sm">{errors.phoneNumber}</div>
-          )}
-        </div>
-
-        <div className="mb-4 relative">
-          <MailIcon className="absolute w-6 h-6 left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
-          <input
-            className="pl-10 w-full px-3 py-2 placeholder-gray-300 border rounded-md bg-F0F7F9"
-            placeholder="Email"
-            value={formData.email}
-            onChange={(e) =>
-              setFormData({ ...formData, email: e.target.value })
-            }
-          />
-          {errors.email && (
-            <div className="text-red-500 text-sm">{errors.email}</div>
-          )}
-        </div>
-
-        <div className="mb-4 relative">
-          <LockClosedIcon className="absolute w-6 h-6 left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
-          <input
-            className="pl-10 w-full px-3 py-2 placeholder-gray-300 border rounded-md bg-F0F7F9"
-            type="password"
-            placeholder="Password"
-            value={formData.password}
-            onChange={(e) =>
-              setFormData({ ...formData, password: e.target.value })
-            }
-          />
-          {errors.password && (
-            <div className="text-red-500 text-sm">{errors.password}</div>
-          )}
-        </div>
-
-        <button
-          onClick={handleSubmit}
-          className="w-full p-2 bg-61BDD5 text-white rounded-md"
-        >
-          Sign Up
-        </button>
-      </div>
-
-      <div className="m-auto w-1/2">
-        {/* Replace with your image */}
         <img
           className="h-full w-full object-cover"
           src={SignUpImg}
