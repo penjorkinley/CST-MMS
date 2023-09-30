5efea5b4fac04ba73be9f3407d0b70a5f94138c8
karzeydema <110452208+karzeydema@users.noreply.github.com>
2023-09-30 17:30:21 +0600

    updated navbar

--- client/src/pages/Feedback.jsx
+++ client/src/pages/Feedback.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import pic from '../assets/KSI.png'
 
 export default function Feedback() {
   return (
-    <div>Feedback</div>
+    <nav className="bg-transparent p-4">
+          <div>
+          <img src={pic}
+          className="h-150 w-150 object-cover"
+          alt="FeedbackPic"
+        /> 
+          </div>
+    </nav>
   )
 }
