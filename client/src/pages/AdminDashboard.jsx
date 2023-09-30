03d8793ec195882e9a36b270e86be60cfccc89af
penjorkinley <penjorkinley77@gmail.com>
2023-10-01 01:56:45 +0600

    update admin dashboard

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import AdminNavbar from "../components/AdminNavbar";
+import AdminNavbar from "../components/adminNavbar";
 import AdminTopNavbar from "../components/AdminTopNavbar";
 
 export default function AdminDashboard() {
   return (
     <div className="flex">
-      <div className="w-1/5">
+      <div className="w-1/6">
         {" "}
         {/* Adjust the width as per your design */}
         <AdminNavbar />
       </div>
-      <div className="w-4/5">
+      <div className="w-5/6">
         {" "}
         {/* Adjust the width as per your design */}
         <AdminTopNavbar />
-       
       </div>
     </div>
   );
