66e47356eb4649adf5d16b3250996310a48ce54c
Kezang Loday <02200150.cst@rub.edu.bt>
2023-09-30 17:12:36 +0600

    about us

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -4,22 +4,46 @@ import AboutUsImg from "../assets/AboutUs.png";
 
 function MyComponent() {
   return (
-    <div className="flex bg-F0F7F9">
+    <div className="flex">
       <div className="w-1/3">
         <img src={AboutUsImg} alt="About Us Image" className="w-full" />
       </div>
       <div className="w-2/3 px-4">
+        <h1>About Us</h1>
+        <p>
+          Welcome to our Mess Management System! We are dedicated to
+          revolutionizing the dining experience on campus, making it more
+          convenient and enjoyable for students and cafeteria staff alike.
+        </p>
         <h2>Mission</h2>
-        <p>Our mission is...</p>
+        <p>
+          At College of Science and Technology, our mission is to provide a
+          seamless dining experience that caters to the diverse preferences and
+          dietary needs of our students. We aim to achieve this by leveraging
+          cutting-edge technology to streamline menu selection, order
+          processing, and feedback collection.
+        </p>
 
         <h2>Vision</h2>
-        <p>Our vision is...</p>
+        <p>
+          We envision a campus where students can easily access nutritious and
+          delicious meals, tailored to their individual tastes, all while
+          ensuring efficient resource allocation and financial transparency for
+          the Mess.
+        </p>
 
-        <h2>Team</h2>
+        <h2>Meet Our Team</h2>
+        <p>
+          Our team is comprised of passionate individuals with a shared
+          commitment to enhancing the dining experience at College of Science
+          and Technology. Together, we bring a diverse range of skills and
+          expertise to ensure the success of this project.
+        </p>
         <ul>
-          <li>Team Member 1</li>
-          <li>Team Member 2</li>
-          {/* Add more team members as needed */}
+          <li>Chimi Dema</li>
+          <li>Purna Badur</li>
+          <li>Mess Uncle 1</li>
+          <li>Mess Uncle 2</li>
         </ul>
       </div>
     </div>
