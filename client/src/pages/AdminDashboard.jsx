82ff5820a51c49e13f812caacbf71c3b67e99e4f
karzeydema <110452208+karzeydema@users.noreply.github.com>
2023-10-01 01:17:02 +0600

    updated

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-export default function Feedback() {
+export default function AdminDashboard() {
   return (
     <div className='flex justify-between items-center bg-cute p-4 '>
 
@@ -11,16 +11,7 @@ export default function Feedback() {
   {/* <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure> */}
   <div className="card-body "><br></br>
     <h2 className="card-title font-semibold text-black text-2xl italic">Rate Your Experience!!!</h2><br></br>
-    <h4 className="text-xl font-semibold text-black">How happy are you with our meal?</h4>
-    <div className="rating rating-lg">
-        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400 "checked />
-        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-    </div ><br></br>
-    <h4 className="text-xl font-semibold text-black">How could we improve?</h4>
-    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-s" /><br></br>
+  
     <div className="card-actions justify-end pb-10">
       <button className="btn bg-black pl-9 w-full max-w-s font-semibold text-white text-xl">Submit</button>
     </div>
