44f0e6f9d81cc767aa4ad80b9e8d5437a96ff3fc
karzeydema <110452208+karzeydema@users.noreply.github.com>
2023-09-30 13:00:06 +0600

    footer done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-red-400">
+
         Hello world!
       </h1>
+      <Footer/>
+        
     </>
   );
 }
