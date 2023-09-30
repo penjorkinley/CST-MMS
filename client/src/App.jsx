c3fcd1051de461ca853febe4966db0ccadc37b79
bhuwan-1 <69762596+bhuwan-1@users.noreply.github.com>
2023-09-30 12:53:02 +0600

    landing page start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
 
+
 function App() {
   return (
     <>
+      
       <h1 className="text-3xl font-bold underline text-red-400">
         Hello world!
       </h1>
---
+++ client/src/components/regularNavbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function regularNavbar() {
+  return <div>regularNavbar</div>;
+}
+
+export default regularNavbar;
