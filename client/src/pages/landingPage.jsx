e3fdff3e97b2ea3fe1cc841f5cf66d87e4ae6a86
bhuwan-1 <69762596+bhuwan-1@users.noreply.github.com>
2023-09-30 17:57:52 +0600

    land update

--- client/src/pages/landingPage.jsx
+++ client/src/pages/landingPage.jsx
@@ -1,7 +1,14 @@
-
+import { useNavigate } from "react-router-dom";
 import land from "../assets/Menu-Background.png";
-import logo from "../assets/Logo.PNG"
+import logo from "../assets/Logo.PNG";
+
 function LandingPage() {
+  const navigate = useNavigate();
+
+  const navigateToSignup = () => {
+    navigate("/signin");
+  };
+
   return (
     <div className="relative h-screen bg-cute">
       <div className="absolute inset-0 flex items-center justify-center">
@@ -12,11 +19,16 @@ function LandingPage() {
         />
       </div>
       <div className="z-10 flex justify-between items-center h-50 bg-transparent w-full pr-8">
-          <img src={logo} alt="logo" className="h-32 w-34"/>
-          <button className="bg-btn h-11 w-40 rounded-md text-white font-semibold">Get Meal</button>
-        </div>
+        <img src={logo} alt="logo" className="h-32 w-34" />
+        <button
+          onClick={navigateToSignup}
+          className="bg-btn h-11 w-40 rounded-md text-white font-semibold"
+        >
+          Get Meal
+        </button>
+      </div>
     </div>
   );
 }
 
-export default LandingPage
+export default LandingPage;
