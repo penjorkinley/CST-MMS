22e77e27720968e880c9379c928ff474b386b778
Kezang Loday <02200150.cst@rub.edu.bt>
2023-09-30 14:41:21 +0600

    about us

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import './AboutUs.css';
+import aboutusImg from "./client/src/assets/AboutUs.png";
 
 function MyComponent() {
   return (
     <div style={{ backgroundColor: 'blue' }}>
-      {/* Your component content here */}
+      {<div className="flex">
+      <div className="w-40">
+        <img src={aboutusImg} alt="About Us Image" className="w-full" />
+      </div>
+      <div className="w-60 px-4">
+        <h2>Mission</h2>
+        <p>Our mission is...</p>
+
+        <h2>Vision</h2>
+        <p>Our vision is...</p>
+
+        <h2>Team</h2>
+        <ul>
+          <li>Team Member 1</li>
+          <li>Team Member 2</li>
+          {/* Add more team members as needed */}
+        </ul>
+      </div>
+    </div>}
     </div>
   );
 }
