ef217194140cd5af38b17543ee60bef7426296eb
Kezang Loday <02200150.cst@rub.edu.bt>
2023-09-30 23:30:01 +0600

    admin nav

--- client/src/components/adminNavbar.jsx
+++ client/src/components/adminNavbar.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import {TbDeviceDesktopAnalytics} from "react-icons/tb";
 import {BiSolidAddToQueue} from "react-icons/bi";
 import {VscFeedback} from "react-icons/vsc";
+
 export default function adminNavbar() {
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-side">
-      
-        <ul className="menu p-4 w-80 min-h-full text-textBlack bg-adminDash">
+        <ul className="menu p-4 w-80 min-h-full text-blackText bg-adminDash">
           <li>
-            <a>Dashboard <TbDeviceDesktopAnalytics/></a>
+            <a className="text-2xl items-center"><TbDeviceDesktopAnalytics/> Dashboard</a>
           </li>
           <li>
-            <a>Add Menu <BiSolidAddToQueue/></a>
+            <a className="text-2xl items-center"><BiSolidAddToQueue/> Add Menu</a>
           </li>
           <li>
-            <a>View Feedback<VscFeedback/> </a>
+            <a className="text-2xl items-center"><VscFeedback/> View Feedback</a>
           </li>
         </ul>
       </div>
