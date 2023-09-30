dbf4f5ae9d153866f67fe601f1920065685b3b47
Kezang Loday <02200150.cst@rub.edu.bt>
2023-09-30 15:51:30 +0600

    about us

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -2,28 +2,24 @@ import AboutUsImg from "../assets/AboutUs.png";
 
 function AboutUs() {
   return (
-    <div style={{ backgroundColor: "blue" }}>
-      {
-        <div className="flex">
-          <div className="w-40">
-            <img src={AboutUsImg} alt="About Us Image" className="w-full" />
-          </div>
-          <div className="w-60 px-4">
-            <h2>Mission</h2>
-            <p>Our mission is...</p>
+    <div className="flex bg-F0F7F9">
+      <div className="w-40">
+        <img src={AboutUsImg} alt="About Us Image" className="w-full" />
+      </div>
+      <div className="w-60 px-4">
+        <h2>Mission</h2>
+        <p>Our mission is...</p>
 
-            <h2>Vision</h2>
-            <p>Our vision is...</p>
+        <h2>Vision</h2>
+        <p>Our vision is...</p>
 
-            <h2>Team</h2>
-            <ul>
-              <li>Team Member 1</li>
-              <li>Team Member 2</li>
-              {/* Add more team members as needed */}
-            </ul>
-          </div>
-        </div>
-      }
+        <h2>Team</h2>
+        <ul>
+          <li>Team Member 1</li>
+          <li>Team Member 2</li>
+          {/* Add more team members as needed */}
+        </ul>
+      </div>
     </div>
   );
 }
