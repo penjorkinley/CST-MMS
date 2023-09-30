9cd9f177d0617c771d2193ecfc87740f0c974d8c
bhuwan-1 <69762596+bhuwan-1@users.noreply.github.com>
2023-09-30 13:06:07 +0600

    regular navbar

---
+++ client/src/components/RegularNav.jsx
@@ -0,0 +1,9 @@
+
+
+export default function RegularNav() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
--- client/src/components/regularNavbar.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function regularNavbar() {
-  return <div>regularNavbar</div>;
-}
-
-export default regularNavbar;
