0c0fc7b28947febc6d4de92a10d1fd16cb71467c
penjorkinley <penjorkinley77@gmail.com>
2023-10-01 16:09:51 +0600

    adminDashboard update

---
+++ client/src/components/ViewFeedback.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ViewFeedback() {
+  return (
+    <div>
+      <h2>View Feedback</h2>
+      <p>See feedback from users here.</p>
+    </div>
+  );
+}
+
+export default ViewFeedback;
