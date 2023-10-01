4a399ae0a762cddbdd9e20447ccbd2d1a7a6ccf6
bhuwan-1 <69762596+bhuwan-1@users.noreply.github.com>
2023-10-01 18:57:49 +0600

    Update FeedbackCard.jsx

--- client/src/components/FeedbackCard.jsx
+++ client/src/components/FeedbackCard.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from "react";
 
-function FeedbackCard({dets}) {
+function FeedbackCard({ dets }) {
   return (
     <div className="card card-compact w-96 h-20 bg-base-100 shadow-2xl mr-2 p-4 ">
-        <p>{dets}</p>
-        <div className="rating ml-auto">
-            <input type="radio" name="rating-1" className="mask mask-star-2 bg-orange-400" />
-            </div>
-    
-  </div>
-  )
+      <p>{dets}</p>
+      <div className="rating ml-auto">
+        <input
+          type="radio"
+          name="rating-1"
+          className="mask mask-star-2 bg-orange-400"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default FeedbackCard
+export default FeedbackCard;
