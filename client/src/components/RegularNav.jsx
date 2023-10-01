82b13c5d563a3bdf10ba99d24bfe7ef991dd3295
penjorkinley <penjorkinley77@gmail.com>
2023-10-01 14:19:52 +0600

    navbar update

--- client/src/components/RegularNav.jsx
+++ client/src/components/RegularNav.jsx
@@ -1,15 +1,9 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom"; // <-- import useLocation
 import { useState } from "react";
 import logo from "../assets/Logo.PNG";
 import { HiOutlineLogout } from "react-icons/hi";
 
 export default function RegularNav() {
-  // Mock-up state to keep track of the user's login status
-  const { isLoggedIn } = useContext(AuthContext);
-
-  // Get the current location
-  const location = useLocation();
-
   return (
     <div className="bg-cute p-4 border-b-4 h-[120px] sticky top-0 z-50">
       <div className="container mx-auto flex justify-between items-center">
@@ -38,41 +32,40 @@ export default function RegularNav() {
             </Link>
           </li>
 
-          {/* Condition for Sign Up button */}
-          {!isLoggedIn && location.pathname !== "/signin" && (
-            <li>
-              <Link
-                to="/signin"
-                className="text-white hover:bg-white text-lg hover:text-black hover:border-blackText hover:border-2 transition-all duration-500 px-5 py-2 rounded-lg bg-buttons border-2"
-              >
-                Sign Up
-              </Link>
-            </li>
-          )}
+          <>
+            {location.pathname !== "/signin" && ( // <-- Conditionally render the Sign Up button
+              <li>
+                <Link
+                  to="/signin"
+                  className="text-white hover:bg-white text-lg hover:text-black hover:border-blackText hover:border-2 transition-all duration-500 px-5 py-2 rounded-lg bg-buttons border-2"
+                >
+                  Sign Up
+                </Link>
+              </li>
+            )}
+            {location.pathname == "/signin" && ( // <-- Conditionally render the Login button
+              <li>
+                <Link
+                  to="/login"
+                  className="text-white hover:bg-white text-lg hover:text-black hover:border-buttons hover:border-2 transition-all duration-500 px-5 py-2 rounded-lg bg-blackText border-2"
+                >
+                  Login
+                </Link>
+              </li>
+            )}
 
-          {/* Condition for Login button */}
-          {!isLoggedIn && location.pathname !== "/login" && (
-            <li>
-              <Link
-                to="/login"
-                className="text-white hover:bg-white text-lg hover:text-black hover:border-buttons hover:border-2 transition-all duration-500 px-5 py-2 rounded-lg bg-blackText border-2"
-              >
-                Login
-              </Link>
-            </li>
-          )}
-
-          {/* Condition for Logout icon */}
-          {isLoggedIn && (
-            <li>
-              <Link
-                to="/"
-                className="flex items-center justify-center mt-[-7px]"
-              >
-                <HiOutlineLogout size={50} />
-              </Link>
-            </li>
-          )}
+            {location.pathname !== "/signin" &&
+              location.pathname !== "/login" && (
+                <li>
+                  <Link
+                    to="/"
+                    className="flex items-center justify-center mt-[-7px]"
+                  >
+                    <HiOutlineLogout size={50} />
+                  </Link>
+                </li>
+              )}
+          </>
         </ul>
       </div>
     </div>
