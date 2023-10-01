fb858bc5fc066669dc5c55954fdce171402caecb
Kezang Loday <02200150.cst@rub.edu.bt>
2023-10-01 14:28:01 +0600

    admin nav changes

--- client/src/components/adminNavbar.jsx
+++ client/src/components/adminNavbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { TbDeviceDesktopAnalytics } from "react-icons/tb";
 import { BiSolidAddToQueue } from "react-icons/bi";
 import { VscFeedback } from "react-icons/vsc";
@@ -11,42 +12,42 @@ export default function adminNavbar() {
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-side">
-        <ul className="menu min-h-full w-72 text-blackText bg-adminDash">
+        <ul className="menu min-h-full w-72 text-blackText bg-adminDash border-r-4">
           <li className=" py-2">
             <p className="text-3xl items-center hover:bg-transparent">Hello, Admin</p>
           </li>
           <hr className="my-4 border-t-2 border-black w-full " />
           <li className="max-w-xs ">
-            <a className="text-2xl items-center py-4 transition duration-300 ease-in-out hover:scale-105">
+            <Link to= "/dashboard" className="text-2xl items-center py-4 transition duration-300 ease-in-out hover:scale-105">
               <TbDeviceDesktopAnalytics className="text-4xl " /> Dashboard
-            </a>
+            </Link>
             
           </li>
           <li>
-            <a className="text-2xl items-center py-4  transition duration-300 ease-in-out hover:scale-105">
+            <Link to= "/addmenu" className="text-2xl items-center py-4  transition duration-300 ease-in-out hover:scale-105">
               <BiSolidAddToQueue className="text-4xl" /> Add Menu
-            </a>
+            </Link>
           </li>
           <li>
-            <a className="text-2xl items-center py-4  transition duration-300 ease-in-out hover:scale-105">
+            <Link to= "/dailyattendent" className="text-2xl items-center py-4  transition duration-300 ease-in-out hover:scale-105">
               <TiTickOutline className="text-4xl"/> Daily Attendent
-            </a>
+            </Link>
           </li>
           <li>
-            <a className="transition duration-300 ease-in-out hover:scale-105 text-2xl items-center py-4 ">
+            <Link to= "adduser" className="transition duration-300 ease-in-out hover:scale-105 text-2xl items-center py-4 ">
               <AiOutlineUserAdd className="text-4xl"/> Add User
-            </a>
+            </Link>
           </li>
           <li>
-            <a className=" transition duration-300 ease-in-out hover:scale-105 text-2xl items-center py-4 ">
+            <Link to= "viewfeedback" className=" transition duration-300 ease-in-out hover:scale-105 text-2xl items-center py-4 ">
               <VscFeedback className="text-4xl" /> View Feedback
-            </a>
+            </Link>
           </li>
           <div className="mt-auto"> {/* This ensures the item sticks to the bottom */}
           <li>
-            <a className="transition duration-300 ease-in-out hover:scale-105 text-2xl items-center py-4">
+            <Link to= "" className="transition duration-300 ease-in-out hover:scale-105 text-2xl items-center py-4">
               <TbLogout className="text-4xl" /> Log Out
-            </a>
+            </Link>
           </li>
         </div>
         </ul>
