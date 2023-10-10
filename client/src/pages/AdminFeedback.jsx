7037bb6768e075397c0b7155e37dd6737dd0cdb3
bhuwan-1 <69762596+bhuwan-1@users.noreply.github.com>
2023-10-11 01:59:46 +0600

    icon rmv

--- client/src/pages/AdminFeedback.jsx
+++ client/src/pages/AdminFeedback.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import FeedbackCard from "../components/FeedbackCard";
 
 export default function Feedback() {
