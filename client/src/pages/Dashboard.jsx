89bbf1ac1c648ae5d890232c70f42b24860c69d4
Kezang Loday <02200150.cst@rub.edu.bt>
2023-10-11 01:24:02 +0600

    dashboard icon

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import DoughnutChart from "../components/DoughnutChart";
 import DashCards from "../components/DashCards";
 import GradientLineGraph from "../components/GradientLineGraph";
-
+import { RiDashboard2Fill } from "react-icons/ri";
 
 
 function Dashboard() {
@@ -20,7 +20,12 @@ function getWeekNumber(date) {
 
   return (
     <div className="overflow-auto">
+      <div className="flex items-center space-x-2 mb-4 pl-16">
+        <RiDashboard2Fill className="text-4xl text-[#3eadcc] mt-1" />
+        <h1 className="text-4xl font-bold">Dashboard</h1>
+      </div>
       <div className="grid grid-cols-3 gap-4 w-auto h-auto pl-16 mt-5">
+        
         <DashCards
           title="Total Number of Students"
           count="1257"
